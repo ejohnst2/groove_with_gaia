831b01c637815606ee02c89ee818f1e39546143e
Elijah Johnston <eli@changeinit.com>
2020-08-22 14:42:30 -1000

    struct init

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("is this working?")
+
+// (window).scroll(function()
+//   {
+//     varscroll=(window).scrollTop()
+//     .document.getElementsByTagName('header')
+//     .css({opacity:0.5+(scroll/200)}).
+//   });
+
