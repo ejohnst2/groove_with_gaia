f339a147d37041c3ec726fb6921df81dad43e6dc
Elijah Johnston <eli@changeinit.com>
2020-10-11 18:58:41 -1000

    middleman init

--- config.rb
+++ config.rb
@@ -1,6 +1,15 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
