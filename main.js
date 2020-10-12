fa782c10220fee10f715ffc510ef6a8273f7d38d
Elijah Johnston <eli@changeinit.com>
2020-10-11 18:44:23 -1000

    github pages test

--- main.js
+++
@@ -1,9 +0,0 @@
-console.log("is this working?")
-
-// $(window).scroll(function() {
-//   var scroll = $(window).scrollTop();
-//   $("header").css({
-//     opacity: 0.5 + (scroll/200)
-//   });
-// });
-
